b30b4973840f4d97e2ef94942fa9b4cd67051b86
Alexandre Vermeersch <alexandrevermeersch@MacBook-Pro.local>
2022-12-05 13:30:56 +0100

    check turned phone (d-none)

--- js/game.js
+++ js/game.js
@@ -5,121 +5,124 @@ let game = document.getElementById('content');
 
 
 function init() {
-    canvas = document.getElementById('canvas');
-    initLevel();
-    world = new World(canvas, keyboard,);
-    mobileBtn();
-    document.getElementById('startscreen-container').classList.add('d-none');
-    document.getElementById('endscreen-container').classList.add('d-none');
-    
+  canvas = document.getElementById('canvas');
+  initLevel();
+  world = new World(canvas, keyboard,);
+  mobileBtn();
+  document.getElementById('startscreen-container').classList.add('d-none');
+  document.getElementById('endscreen-container').classList.add('d-none');
+
 }
 
 function restart() {
-    location.reload();
-  }
+  location.reload();
+}
 
 
 /* Alternative (quick and dirty), um alle Intervalle zu beenden. */
 function clearAllIntervals() {
-    for (let i = 1; i < 9999; i++) window.clearInterval(i);
-  };
+  for (let i = 1; i < 9999; i++) window.clearInterval(i);
+};
 
 window.addEventListener('keydown', (event) => {
-    if (event.keyCode == 39) {
-        keyboard.right = true;
-    }
-    if (event.keyCode == 37) {
-        keyboard.left = true;
-    }
-    if (event.keyCode == 38) {
-        keyboard.up = true;
-    }
-    if (event.keyCode == 40) {
-        keyboard.down = true;
-    }
-    if (event.keyCode == 32) {
-        keyboard.space = true;
-    }
-    if (event.keyCode == 68) {
-        keyboard.D = true;
-    }
+  if (event.keyCode == 39) {
+    keyboard.right = true;
+  }
+  if (event.keyCode == 37) {
+    keyboard.left = true;
+  }
+  if (event.keyCode == 38) {
+    keyboard.up = true;
+  }
+  if (event.keyCode == 40) {
+    keyboard.down = true;
+  }
+  if (event.keyCode == 32) {
+    keyboard.space = true;
+  }
+  if (event.keyCode == 68) {
+    keyboard.D = true;
+  }
 })
 
 window.addEventListener('keyup', (event) => {
-    if (event.keyCode == 39) {
-        keyboard.right = false;
-    }
-    if (event.keyCode == 37) {
-        keyboard.left = false;
-    }
-    if (event.keyCode == 38) {
-        keyboard.up = false;
-    }
-    if (event.keyCode == 40) {
-        keyboard.down = false;
-    }
-    if (event.keyCode == 32) {
-        keyboard.space = false;
-    }
-    if (event.keyCode == 68) {
-        keyboard.D = false;
-    }
+  if (event.keyCode == 39) {
+    keyboard.right = false;
+  }
+  if (event.keyCode == 37) {
+    keyboard.left = false;
+  }
+  if (event.keyCode == 38) {
+    keyboard.up = false;
+  }
+  if (event.keyCode == 40) {
+    keyboard.down = false;
+  }
+  if (event.keyCode == 32) {
+    keyboard.space = false;
+  }
+  if (event.keyCode == 68) {
+    keyboard.D = false;
+  }
 })
 
 /* Mobile Character Control*/
 
- function mobileBtn() {
-    document.getElementById('walk-button-left').addEventListener('touchstart', (e) => {
-      e.preventDefault();
-      keyboard.left = true;
-    });
-  
-    document.getElementById('walk-button-left').addEventListener('touchend', (e) => {
-      e.preventDefault();
-      keyboard.left = false;
-    });
-  
-    document.getElementById('walk-button-right').addEventListener('touchstart', (e) => {
-      e.preventDefault();
-      keyboard.right = true;
-    });
-  
-    document.getElementById('walk-button-right').addEventListener('touchend', (e) => {
-      e.preventDefault();
-      keyboard.right = false;
-    });
-  
-    document.getElementById('jump-button').addEventListener('touchstart', (e) => {
-      e.preventDefault();
-      keyboard.space = true;
-    });
-  
-    document.getElementById('jump-button').addEventListener('touchend', (e) => {
-      e.preventDefault();
-      keyboard.space = false;
-    });
-  
-    document.getElementById('shoot-button').addEventListener('touchstart', (e) => {
-      e.preventDefault();
-      keyboard.D = true;
-    });
-  
-    document.getElementById('shoot-button').addEventListener('touchend', (e) => {
-      e.preventDefault();
-      keyboard.D = false;
-    });
-  }
-  
-  
-   /*convert canvas to fullscreen*/
-   
-  function fullscreen() {
-    document.getElementById('Fullscreen').requestFullscreen();
-  }
+function mobileBtn() {
+  document.getElementById('walk-button-left').addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    keyboard.left = true;
+  });
 
-  function checkScreen(){
-   setInterval(() =>{ if (window.innerHeight < window.innerWidth) {
+  document.getElementById('walk-button-left').addEventListener('touchend', (e) => {
+    e.preventDefault();
+    keyboard.left = false;
+  });
+
+  document.getElementById('walk-button-right').addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    keyboard.right = true;
+  });
+
+  document.getElementById('walk-button-right').addEventListener('touchend', (e) => {
+    e.preventDefault();
+    keyboard.right = false;
+  });
+
+  document.getElementById('jump-button').addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    keyboard.space = true;
+  });
+
+  document.getElementById('jump-button').addEventListener('touchend', (e) => {
+    e.preventDefault();
+    keyboard.space = false;
+  });
+
+  document.getElementById('shoot-button').addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    keyboard.D = true;
+  });
+
+  document.getElementById('shoot-button').addEventListener('touchend', (e) => {
+    e.preventDefault();
+    keyboard.D = false;
+  });
+}
+
+
+/*convert canvas to fullscreen*/
+
+function fullscreen() {
+  document.getElementById('Fullscreen').requestFullscreen();
+}
+
+function checkScreen() {
+  setInterval(() => {
+    if (window.innerHeight < window.innerWidth) {
       document.getElementById('turnPhone').classList.add('d-none');
+    } else {
+      document.getElementById('turnPhone').classList.remove('d-none');
     }
-    },1000)
-  }
+  }, 10)
+}
